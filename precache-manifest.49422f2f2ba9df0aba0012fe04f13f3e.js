9b85b321ebc009a94534bfbec8d34222f5715bf5
konoplitskii <s.konoplitsky@yandex.com>
2023-03-09 08:42:30 +0300

    Initial commit

---
+++ precache-manifest.49422f2f2ba9df0aba0012fe04f13f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a32364c095cc7c7702ff",
+    "url": "/calendarE2/static/css/main.271a3735.chunk.css"
+  },
+  {
+    "revision": "a32364c095cc7c7702ff",
+    "url": "/calendarE2/static/js/main.a32364c0.chunk.js"
+  },
+  {
+    "revision": "33a71eb3fe4a6dd0cdad",
+    "url": "/calendarE2/static/js/1.33a71eb3.chunk.js"
+  },
+  {
+    "revision": "c7c2cf9f010d72f15440",
+    "url": "/calendarE2/static/js/runtime~main.c7c2cf9f.js"
+  },
+  {
+    "revision": "0df6d1e4f40dd299bd22d4e6e9cb6ceb",
+    "url": "/calendarE2/index.html"
+  }
+];
